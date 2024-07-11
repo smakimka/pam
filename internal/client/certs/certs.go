bdbbbbf9e6fe9ea30270caffde5499b40be4441d
smakimka <smakimka@gmail.com>
2024-07-11 13:59:43 +0300

    workflow 1

--- internal/client/certs/certs.go
+++ internal/client/certs/certs.go
@@ -3,19 +3,16 @@ package certs
 import (
 	"crypto/tls"
 	"crypto/x509"
+	_ "embed"
 	"fmt"
-	"os"
 
 	"google.golang.org/grpc/credentials"
 )
 
-func LoadTLSCredentials() (credentials.TransportCredentials, error) {
-	// Load certificate of the CA who signed server's certificate
-	pemServerCA, err := os.ReadFile("certs/ca-cert.pem")
-	if err != nil {
-		return nil, err
-	}
+//go:embed ca-cert.pem
+var pemServerCA []byte
 
+func LoadTLSCredentials() (credentials.TransportCredentials, error) {
 	certPool := x509.NewCertPool()
 	if !certPool.AppendCertsFromPEM(pemServerCA) {
 		return nil, fmt.Errorf("failed to add server CA's certificate")
